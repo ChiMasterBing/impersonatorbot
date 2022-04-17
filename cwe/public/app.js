eddb9cfe0a8baaaf21317e175f6983179903544d
scienceqiu <scienceqiu@gmail.com>
2022-04-17 03:30:04 -0400

    updated to allow for johnny orz

--- cwe/public/app.js
+++ cwe/public/app.js
@@ -1,26 +1,58 @@
-document.getElementById('pp').addEventListener('submit', (token) => {
-        token.preventDefault();
-        var tok = document.getElementById('pp').elements[0].value;
-        console.log(tok);
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", 'http://127.0.0.1:5000/result', true);
-        console.log("HERE");
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
 
-        console.log("HERE");
+
+function send(){
+        var xhr = new XMLHttpRequest();
+        var url = "johnny's pro url";
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xhr.onreadystatechange = function() { // Call a function when the state changes.
             if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
                 // Request finished. Do processing here.
             }
         }
-        var cid;
-        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-            cid= tabs[0].url.split("/").pop();
-        console.log(cid);
-                //cid = "844600766575542362";
-         payload = "cid="+cid+"&token="+tok;
-                console.log(payload);
+        var payload= document.getElementById('pp').elements[0].value;
         xhr.send(payload);
-            // use `url` here inside the callback because it's asynchronous!
-        });
+        var temp = httpGet(url);
+        document.getElementById("p1").innerHTML = temp;
+}
+
+var yes = false;
+document.getElementById('pp').addEventListener('submit', (token) => {
+        yes = true;
+        if(yes){
+                token.preventDefault();
+                var tok = document.getElementById('pp').elements[0].value;
+                console.log(tok);
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", 'http://127.0.0.1:5000/result', true);
+                console.log("HERE");
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+                console.log("HERE");
+                xhr.onreadystatechange = function() { // Call a function when the state changes.
+                    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                        // Request finished. Do processing here.
+                    }
+                }
+                var cid;
+                chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+                    cid= tabs[0].url.split("/").pop();
+                console.log(cid);
+                        //cid = "844600766575542362";
+                 payload = "cid="+cid+"&token="+tok;
+                        console.log(payload);
+                xhr.send(payload);
+                document.getElementById("p1").innerHTML = "Say something";
+                    // use `url` here inside the callback because it's asynchronous!
+                });
+        }else{
+                sendget();
+        }
 });
